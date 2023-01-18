8f20bc58ad65e4c1c5beba414c77246164eb5672
alex-killer <onefrank@bk.ru>
2023-01-18 08:23:14 +0700

    Initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'mysql',
+        'port' => 3306,
+        'dbname' => 'test',
+        'charset' => 'utf8mb4'
+    ]
+];
---
+++ controllers/notes/show.php
@@ -0,0 +1,16 @@
+<?php
+
+$config = require('config.php');
+$db = new Database($config['database']);
+
+$heading = 'Note';
+$currentUserId = 2;
+
+$note = $db->query('select * from notes where id = :id', [
+    'id' => $_GET['id']
+])->findOrFail();
+
+authorize($note['user_id'] == $currentUserId);
+
+
+require "views/notes/show.view.php";
